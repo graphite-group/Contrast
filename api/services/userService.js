490a9ebe200757335abce1cb36f140af19498ace
Adam Deibert <ardeibert@gmail.com>
2014-04-04 16:22:48 -0700

    updateUser is now passing preliminary ad-hoc tests

--- api/services/userService.js
+++ api/services/userService.js
@@ -34,7 +34,7 @@ module.exports = {
 //returns true if node is successfully updated --- false if it is not found
   updateUser: function(userId, updateData, callback){
     //first need to read node to get old properties
-    var user = this.fetchUserById(userId)
+    var a = this.fetchUserById(userId)
     .then(function(node){
 
       for(var key in updateData){
@@ -42,8 +42,16 @@ module.exports = {
       }
       console.log(node);
       return node;
+    })
+    .then(function(node){
+      return db.updateNodeAsync(node._id, node, function(err, updated){
+        if(err){
+          throw err;
+        }
+        return updated;
+      });
     });
-    // .then();
+
     //then extend with the new properties in updateData
     // var a = db.updateNodeAsync(userId, updateData);
     // if(typeof callback === 'function'){
@@ -51,6 +59,11 @@ module.exports = {
     // } else {
     //   return a;
     // }
+    if(typeof callback === 'function'){
+      a.then(callback.bind(this, null)).catch(callback);
+    } else {
+      return a;
+    }
   },
 
   fetchUserById: function(userId, callback){
@@ -85,6 +98,6 @@ module.exports = {
 // };
 
 // module.exports.createUser(userDataTest, function(node){console.log(node);});
-// module.exports.fetchUserById(542).then(function(node){console.log(node);});
-// module.exports.updateUser(45, {name:"harish"}); //.then(function(node){console.log(node);});
-module.exports.deleteUser(542).then(function(node){console.log(node);});
+module.exports.fetchUserById(544).then(function(node){console.log(node);});
+// module.exports.updateUser(544, {name:"harish"}).then(function(node){console.log("last Console Log",node);});
+// module.exports.deleteUser(542).then(function(node){console.log(node);});
