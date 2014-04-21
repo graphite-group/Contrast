bb47ae5d06e52f779b48eecd2b153bbb3692944e
Naman Goel <naman34@gmail.com>
2014-04-21 09:59:18 -0700

    fixed image service

--- api/services/commentService.js
+++ api/services/commentService.js
@@ -10,20 +10,61 @@ db = Promise.promisifyAll(db);
 
 module.exports = {
 
-  addComment : function(userId, imageId, text) {
+  addComment : function(userId, imageId, text, callback) {
 
-    return db.cypherQueryAsync(
+    var a =
+    db.cypherQueryAsync(
       'START user=node(' + userId + '), image=node(' + imageId + ') \n' +
-      'CREATE UNIQUE (user)-[:COMMENTED]->(c:comment)<-[:HAS_COMMENT]-(image) \n' +
-      'RETURN user, comment, image'
-    );
+      'CREATE UNIQUE (user)-[:COMMENTED]->(c:comment {text: "' + text + '", createdAt: ' + JSON.stringify(new Date()) + '})<-[:HAS_COMMENT]-(image) \n' +
+      'RETURN c, user, image'
+    )
+    .then(function(result){return result.data[0];})
+    .spread(function(comment, user, image){
+      comment.user = user;
+      comment.image = image;
 
+      sails.io.sockets.emit('comment', {
+        data: comment,
+        id: comment._id,
+        imageId: image._id,
+        verb: 'create',
+        createdAt: comment.createdAt,
+        updatedAt: new Date()
+      });
+
+      return comment;
+    });
+
+    if(typeof callback === 'function'){
+      a.then(callback.bind(this, null)).catch(callback);
+    } else {
+      return a;
+    }
+  },
+
+  getComments: function(imageId, callback){
+    var a =
+    db.cypherQueryAsync(
+      'START image=node(26758) \n' +
+      'MATCH (u:user)-[:COMMENTED]->(c:comment)<-[:HAS_COMMENT]-(image) \n' +
+      'RETURN c, u'
+    )
+    .then(function(result){return result.data;})
+    .map(function(result){
+      var comment = result[0];
+      var user = result[1];
+      comment.user = user;
+      return comment;
+    });
+
+    if(typeof callback === 'function'){
+      a.then(callback.bind(this, null)).catch(callback);
+    } else {
+      return a;
+    }
   }
 
 };
 
-// b.cypherQueryAsync(
-//       'START user=node(), image=node() \n' +
-//       'CREATE UNIQUE (user)-[:COMMENTED]->(c:comment)<-[:HAS_COMMENT]-(image) \n' +
-//       'RETURN user, comment, image'
-//     ).then(console.log.bind(console))
+
+//.then(console.log.bind(console));
