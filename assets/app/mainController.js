53cbba8dbe65ac41ae2245eb60a92039a1f0b8df
Adam Deibert <ardeibert@gmail.com>
2014-04-20 22:02:06 -0700

    notificationCtrl listening for update events on 'challenge' socket and console logging for ended challenges

--- assets/app/mainController.js
+++ assets/app/mainController.js
@@ -2,16 +2,52 @@
 
 module.exports = function(app, socket){
 
-  user = socket.getAsync('/isLoggedIn').then(function(response){
-    if(!!response.success){
-      return response.data;
-    }else{
-      return {};
-    }
-  });
-
   app
-    .controller('mainController', ['$scope', function($scope){
+    .controller('notificationCtrl', ['$scope', 'MainService', function($scope, MainService){
       
+      $scope.listener = function(event){
+        if(event.verb === 'create'){
+          // $scope.images.push(event.data);
+        }
+        // console.log(event);
+        if(event.verb === 'update'){
+          var ended = event.data.labels.indexOf('ended') !== -1;
+          var opponentId = parseFloat(event.data.opponent._id);
+          var challengerId = parseFloat(event.data.challenger._id);
+          
+          // console.log('==========mainNotificationCtrl update happening===========', event.data);
+          
+          //get the logged in user via promise from main service
+          if(ended){
+            MainService.isLoggedIn().then(function(user){
+              var curUserId = parseFloat(user.id);
+
+              console.log('scope profile id fireing', curUserId);
+              if(curUserId === opponentId && curUserId === challengerId){
+                
+                console.log('you are both the challenger and the opponent');
+
+              } else if(curUserId === opponentId){
+
+                console.log('you are the opponent');
+
+              } else if(curUserId === challengerId){
+
+                console.log('you are the challenger');
+
+              }
+            });
+
+          }
+        //   if(parseInt($scope.profileId) === parseInt(event.id)){
+        //     for(var key in event.data){
+        //       $scope.user[key] = event.data[key];
+        //     }
+        //     $scope.$apply();
+        //   }
+        }
+      };
+
+      socket.on('challenge', $scope.listener);
     }]);
 };
